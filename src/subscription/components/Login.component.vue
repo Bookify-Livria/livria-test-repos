a1338385a0cacf45f4633d0735bc48f4f18243d8
ainhoa <ainhoa.c.garay@gmail.com>
2025-05-14 08:43:54 -0500

    updates user & notifications implementation

--- src/subscription/components/Login.component.vue
+++ src/subscription/components/Login.component.vue
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { ref } from 'vue';
 import 'primeicons/primeicons.css';
 import LanguageSwitcher from "../../public/components/language-switcher.component.vue";
+import {notifyEvent} from "../../public/shared-services/to-notify.js";
 
 const value1 = '';
 const value2 = '';
@@ -66,21 +67,18 @@ export default {
         return null;
       }
     },
-
     goToHome() {
       this.$router.push('/home');
     },
-
     goToRegister() {
       this.$router.push('/register');
     },
-
-
     async handleLogin(valueA, valueB) {
       const matchedUser = await this.validateLogin(valueA, valueB);
       if (matchedUser && valueA!=='' && valueB!=='') {
         await this.createLogin(matchedUser.id, valueA, valueB);
         this.showLogin();
+        await notifyEvent("login");
         this.goToHome();
       } else {
         this.showFail();
